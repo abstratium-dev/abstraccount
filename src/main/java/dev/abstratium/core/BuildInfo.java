3e9e974e32aa5fbf1b5a8b38f26ca03e00632654
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2026-01-27 22:40:18 +0100

    improving integration with windsurf

--- src/main/java/dev/abstratium/core/BuildInfo.java
+++ src/main/java/dev/abstratium/core/BuildInfo.java
@@ -18,7 +18,7 @@ public final class BuildInfo {
      * This value is automatically updated by .git/hooks/pre-commit
      * DO NOT EDIT MANUALLY - changes will be overwritten
      */
-    public static final String BUILD_TIMESTAMP = "2026-01-26T20:30:12Z";
+    public static final String BUILD_TIMESTAMP = "2026-01-27T21:40:18Z";
     
     private BuildInfo() {
         // Utility class - prevent instantiation
