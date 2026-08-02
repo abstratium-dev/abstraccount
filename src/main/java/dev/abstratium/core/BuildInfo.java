5fcda1e90e09abde879dc5414e930fdac374d1a0
Ant Kutschera <1108507+maxant@users.noreply.github.com>
2026-08-02 17:24:44 +0200

    adding info dialog

--- src/main/java/dev/abstratium/core/BuildInfo.java
+++ src/main/java/dev/abstratium/core/BuildInfo.java
@@ -18,7 +18,7 @@ public final class BuildInfo {
      * This value is automatically updated by .git/hooks/pre-commit
      * DO NOT EDIT MANUALLY - changes will be overwritten
      */
-    public static final String BUILD_TIMESTAMP = "2026-08-01T21:31:13Z";
+    public static final String BUILD_TIMESTAMP = "2026-08-02T15:24:44Z";
     
     private BuildInfo() {
         // Utility class - prevent instantiation
